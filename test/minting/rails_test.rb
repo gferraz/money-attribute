fbb8bfa69fdee9787e7c3bf482ec01aafbbb977b
Gilson Ferraz <gilson@cesar.etc.br>
2026-05-28 10:57:08 -0300

    remove inadequated test

--- test/minting/rails_test.rb
+++ test/minting/rails_test.rb
@@ -15,9 +15,6 @@ module Mint
 
     test 'enable currencies configuration' do
       assert_equal :all, Mint.config.enabled_currencies
-      assert_equal %w[AUD BRL CAD CHF CNY EUR GBP JPY MXN NZD PEN SEK USD],
-                   Mint.valid_currency_codes
-
       assert_equal Mint.currency('BRL'), Mint.assert_valid_currency!('BRL')
       assert_equal Mint.currency('USD'), Mint.assert_valid_currency!('USD')
       assert_raises(ArgumentError) {  Mint.assert_valid_currency!('XPX') }

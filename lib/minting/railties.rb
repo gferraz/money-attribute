d3255dab4bf88d089faab6e692edc3bb543a367c
Gilson Ferraz <gilson@cesar.etc.br>
2026-06-17 13:18:51 -0300

    Add I18n support

--- lib/minting/railties.rb
+++ lib/minting/railties.rb
@@ -6,7 +6,21 @@ module Mint
       require 'generators/minting/initializer_generator'
     end
 
+    initializer 'minting-rails.i18n' do |app|
+      locale_dir = File.expand_path('../../config/locales', __dir__)
+      app.config.i18n.load_path += Dir[File.join(locale_dir, '*.yml')]
+    end
+
     config.after_initialize do
+      Mint.locale_backend = -> {
+        fmt = I18n.t('number.currency.format', default: {})
+        {
+          decimal: fmt[:separator],
+          thousand: fmt[:delimiter],
+          format: fmt[:format]&.gsub('%n', '%<amount>f')&.gsub('%u', '%<symbol>s')
+        }
+      }
+
       Array(Mint.config.added_currencies).each do |currency_data|
         if currency_data.respond_to?(:values_at)
           code = currency_data[:currency] || currency_data['currency']
--- test/minting/rails_test.rb
+++ test/minting/rails_test.rb
@@ -4,6 +4,16 @@ require 'test_helper'
 
 module Mint
   class RailsTest < ActiveSupport::TestCase
+    setup do
+      @original_locale_backend = Mint.locale_backend
+      @original_locale = I18n.locale
+    end
+
+    teardown do
+      Mint.locale_backend = @original_locale_backend
+      I18n.locale = @original_locale
+    end
+
     test 'it has a version number' do
       assert Mint::MoneyAttribute::VERSION
       assert Minting::VERSION
@@ -19,6 +29,46 @@ module Mint
       with_mint_config(default_currency: 'USD') do
         assert_equal 'USD', Mint.default_currency.code
       end
+    end
+
+    test 'locale backend is configured and returns defaults' do
+      assert_respond_to Mint.locale_backend, :call
+      result = Mint.locale_backend.call
+      assert_kind_of Hash, result
+      assert_includes result.keys, :decimal
+      assert_includes result.keys, :thousand
+      assert_includes result.keys, :format
+    end
+
+    test 'locale backend reads from Rails I18n for current locale' do
+      I18n.locale = :en
+
+      result = Mint.locale_backend.call
+      assert_equal '.', result[:decimal]
+      assert_equal ',', result[:thousand]
+      assert_equal '%<symbol>s%<amount>f', result[:format]
+    end
+
+    test 'format string is mapped from Rails to minting syntax' do
+      Mint.locale_backend = -> {
+        { decimal: ',', thousand: '.', format: '%<amount>f %<symbol>s' }
+      }
+      result = Mint.locale_backend.call
+      assert_equal '%<amount>f %<symbol>s', result[:format]
+
+      Mint.locale_backend = -> {
+        { decimal: '.', thousand: ',', format: '%<symbol>s%<amount>f' }
+      }
+      result = Mint.locale_backend.call
+      assert_equal '%<symbol>s%<amount>f', result[:format]
+    end
+
+    test 'locale backend formats money with locale-aware separators' do
+      Mint.locale_backend = -> {
+        { decimal: ',', thousand: '.', format: '%<symbol>s %<amount>f' }
+      }
+      money = Mint.money(1234.56, 'USD')
+      assert_equal '$ 1.234,56', money.to_s
     end
 
     test 'configuration has defaults without Active Support configurable' do

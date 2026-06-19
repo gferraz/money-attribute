8bbca49dfd0cd681588e6beab90800529209757f
Gilson Ferraz <gilson@cesar.etc.br>
2026-06-19 18:47:17 -0300

    organize tests in smaller files

--- test/minting/money_attribute_test.rb
+++ test/minting/money_attribute_test.rb
@@ -4,106 +4,6 @@ require 'test_helper'
 
 module Mint
   class MoneyAttributeTest < ActiveSupport::TestCase
-    test 'Money attribute is enabled' do
-      assert Offer.attribute :price
-    end
-
-    test 'aggregated money attribute updates mapped attributes' do
-      offer = Offer.new(price: 12.dollars)
-
-      assert_equal 12.dollars, offer.price
-      assert_equal 12, offer.price_amount
-      assert_equal 'USD', offer.price_currency
-    end
-
-    test 'aggregated money attribute parses any amount to the default currency' do
-      offer = Offer.new(price: '12')
-
-      assert_equal Mint.money(12, Mint.default_currency), offer.price
-      assert_equal 12, offer.price_amount
-      assert_equal Mint.default_currency.code, offer.price_currency
-    end
-
-    test 'aggregated money attribute is saved correctly' do
-      offer = Offer.new(price: 15.euros)
-      offer.save!
-
-      assert_equal offer.price, Offer.where(price: 15.euros).first.price
-      assert_equal offer.price, Offer.where(price_amount: 15.00, price_currency: 'EUR').first.price
-      assert_empty Offer.where(price: 15.dollars)
-    end
-
-    test 'aggregated money attribute reads from mapped amount and currency columns' do
-      offer = Offer.new(price_amount: 17.01, price_currency: 'USD')
-
-      assert_equal 17.01.dollars, offer.price
-    end
-
-    test 'aggregated money attribute allows nil values' do
-      offer = Offer.new(price: nil)
-
-      assert_nil offer.price
-      assert_nil offer.price_amount
-      assert_nil offer.price_currency
-    end
-
-    test 'aggregated money attribute with integer amount column' do
-      transaction = FinancialTransaction.new(amount: 45.34.dollars)
-
-      assert_equal 45.34.dollars, transaction.amount
-      assert_equal 4534, transaction[:amount]
-      assert_equal 'USD', transaction.currency
-    end
-
-    test 'aggregated money attribute with integer column saves and reloads' do
-      transaction = FinancialTransaction.new(amount: 12.50.dollars, description: 'test')
-      transaction.save!
-
-      reloaded = FinancialTransaction.find(transaction.id)
-
-      assert_equal 12.50.dollars, reloaded.amount
-      assert_equal 'test', reloaded.description
-    end
-
-    test 'aggregated money attribute with integer column queries by raw columns' do
-      FinancialTransaction.create!(amount: 5.euros, description: 'eur')
-      FinancialTransaction.create!(amount: 10.dollars, description: 'usd')
-
-      found = FinancialTransaction.where(amount: 5.euros).first
-
-      assert_equal 5.euros, found.amount
-      assert_equal 'eur', found.description
-    end
-
-    test 'aggregated money attribute with integer column handles different currencies' do
-      transaction = FinancialTransaction.new(amount: 7.euros, description: 'multi')
-
-      assert_equal 7.euros, transaction.amount
-      assert_equal 'EUR', transaction.currency
-
-      transaction.save!
-      reloaded = FinancialTransaction.find(transaction.id)
-
-      assert_equal 7.euros, reloaded.amount
-    end
-
-    test 'aggregated money attribute supports custom mappings' do
-      mapped_offer = Class.new(ApplicationRecord) do
-        self.table_name = 'offers'
-
-        money_attribute :cost, mapping: {
-          amount: :price_amount,
-          currency: :price_currency
-        }
-      end
-
-      offer = mapped_offer.new(cost: 19.euros)
-
-      assert_equal 19.euros, offer.cost
-      assert_equal 19, offer.price_amount
-      assert_equal 'EUR', offer.price_currency
-    end
-
     test 'aggregated money attribute requires backing amount and currency columns' do
       invalid_offer = Class.new(ApplicationRecord) do
         self.table_name = 'offers'
@@ -113,38 +13,6 @@ module Mint
       assert_raises(ArgumentError) do
         invalid_offer.money_attribute :cost, mapping: { price_amount: :amount }
       end
-    end
-
-    test 'composite money attribute reads from directly written columns' do
-      offer = Offer.new(price_amount: 25, price_currency: 'EUR')
-
-      assert_equal 25.euros, offer.price
-    end
-
-    test 'money attribute uses :to_d extractor for decimal columns' do
-      assert_equal :to_d, Offer.amount_extractor_for(:price_amount)
-    end
-
-    test 'money attribute uses :fractional extractor for integer columns' do
-      assert_equal :fractional, FinancialTransaction.amount_extractor_for(:amount)
-    end
-
-    test 'composite money attribute accepts zero' do
-      offer = Offer.new(price: 0.dollars)
-
-      assert_equal 0.dollars, offer.price
-      offer.save!
-
-      assert_equal 0.dollars, offer.reload.price
-    end
-
-    test 'composite money attribute accepts negative values' do
-      offer = Offer.new(price: -5.50.dollars)
-
-      assert_equal(-5.50.dollars, offer.price)
-      offer.save!
-
-      assert_equal(-5.50.dollars, offer.reload.price)
     end
 
     test 'find_money_attributes infers currency column from convention when mapping only amount' do
@@ -179,6 +47,10 @@ module Mint
       assert_includes error.message, 'Expected: cost_amount, cost_currency'
     end
 
+    test 'money attribute uses :to_d extractor for decimal columns' do
+      assert_equal :to_d, Offer.amount_extractor_for(:price_amount)
+    end
+
     test 'parse keeps money values unchanged' do
       parser = MoneyAttribute::Parser.new('USD')
 
@@ -188,34 +60,6 @@ module Mint
       assert_equal(-25.34.dollars, parser.parse('-25.34 EUR'))
       assert_nil MoneyAttribute::Parser.new.parse(nil, 'USD')
       assert_raises(TypeError) { parser.parse(23.euros, 'USD') }
-    end
-
-    test 'aggregated money attribute partial custom mapping for currency only' do
-      mapped = Class.new(ApplicationRecord) do
-        self.table_name = 'offers'
-
-        money_attribute :price, mapping: { currency: :price_currency }
-      end
-
-      offer = mapped.new(price: 19.euros)
-
-      assert_equal 19.euros, offer.price
-      assert_equal 19, offer.price_amount
-      assert_equal 'EUR', offer.price_currency
-    end
-
-    test 'aggregated money attribute partial custom mapping for amount only' do
-      mapped = Class.new(ApplicationRecord) do
-        self.table_name = 'offers'
-
-        money_attribute :price, mapping: { amount: :price_amount }
-      end
-
-      offer = mapped.new(price: 19.euros)
-
-      assert_equal 19.euros, offer.price
-      assert_equal 19, offer.price_amount
-      assert_equal 'EUR', offer.price_currency
     end
   end
 end

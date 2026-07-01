0fbcc16c7872c3bc2f6e9e23646d6d454faafde2
Gilson Ferraz <gilson@cesar.etc.br>
2026-07-01 08:47:44 -0300

    check if to_money exists, before removing it.

--- lib/money_attribute/core_ext.rb
+++ lib/money_attribute/core_ext.rb
@@ -2,14 +2,14 @@
 
 # :nodoc
 class Numeric
-  remove_method :to_money
+  remove_method :to_money if method_defined?(:to_money)
 
   def to_money(currency = MoneyAttribute.default_currency) = Mint.money(self, currency)
 end
 
 # :nodoc
 class String
-  remove_method :to_money
+  remove_method :to_money if method_defined?(:to_money)
 
   def to_money(currency = MoneyAttribute.default_currency) = Mint.parse(self, currency)
 end

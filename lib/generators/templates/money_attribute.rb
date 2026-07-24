d343d9907d25ff1885f9cbd00db88e01656d64fd
Gilson Ferraz <gilson@cesar.etc.br>
2026-07-24 09:51:33 -0300

    Add instructions in initializer to register crypto currencies

--- lib/generators/templates/money_attribute.rb
+++ lib/generators/templates/money_attribute.rb
@@ -2,18 +2,29 @@
 # frozen_string_literal: true
 
 MoneyAttribute.configure do |config|
-  # Register a custom currency
-  #
-  # Example:
-  #   config.added_currencies = [
-  #     {currency: 'ZCRC', subunit: 2, symbol: '₡'},
-  #    {currency: 'ZNGN', subunit: 3, symbol: '₦'}
-  #   ]
-  config.added_currencies = []
-
   # To set the default currency
   #
   # It must be a registered currency
   #
   config.default_currency = 'USD'
+
+  # Register built-in crypto currencies
+  #
+  # 1. Register specific crypto currencies:
+  #   Mint::Currency.register_crypto('BTC', 'ETH')
+  #
+  # 2. Register all built-in crypto currencies at once:
+  #   Mint::Currency.register_all_crypto
+  #
+  # See available crypto currencies:
+  #   Mint::Currency.crypto_currencies
+
+  # Register a custom currency
+  #
+  # Example:
+  #   config.added_currencies = [
+  #    {currency: 'ZCRC', subunit: 2, symbol: '₡'},
+  #    {currency: 'ZNGN', subunit: 3, symbol: '₦'}
+  #   ]
+  config.added_currencies = []
 end

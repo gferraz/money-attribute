0d86f26c67943f56f29452f9478288c193a9812e
Gilson Ferraz <gilson@cesar.etc.br>
2026-07-24 08:02:49 -0300

    move core_ext to numeric

---
+++ lib/money_attribute/core_ext/numeric.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# :nodoc:
+class Numeric
+  remove_method :to_money if method_defined?(:to_money)
+
+  def to_money(currency = MoneyAttribute.default_currency) = Mint.money(self, currency)
+end

6b01a557bb7af34d350610df8ba37094b02fe9d5
Gilson Ferraz <gilson@cesar.etc.br>
2026-07-28 11:52:16 -0300

    use AR connection.quote for tests made directly with SQL

--- test/money_attribute/financial_transaction_test.rb
+++ test/money_attribute/financial_transaction_test.rb
@@ -132,9 +132,9 @@ class FinancialTransactionTest < ActiveSupport::TestCase
   test 'tax uses bigint column storing fractional (cents)' do
     transaction = FinancialTransaction.create!(tax: 12.34.reais)
 
-    assert_equal 1234, FinancialTransaction.connection.select_all(
-      'SELECT tax FROM financial_transactions WHERE id = ?', 'SQL', [transaction.id]
-    ).first['tax']
+    assert_equal 1234, FinancialTransaction.connection.select_value(
+      "SELECT tax FROM financial_transactions WHERE id = #{FinancialTransaction.connection.quote(transaction.id)}"
+    )
   end
 
   test 'column inference is independent of money_attribute declaration order' do
